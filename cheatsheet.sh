a5c0025e0cfcb3020147351cf1e4291c4d10c4e4
Jose Moreno <jomore@microsoft.com>
2019-12-03 10:32:59 +0100

    quotes for queries (zsh)

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -30,7 +30,7 @@ az network vnet subnet create -g $rg --vnet-name $vnet -n $batchsubnet --address
 # External LB
 az network public-ip create -g $rg -n $pip --sku $lbsku --allocation-method Static
 az network lb create -g $rg -n $extlb --sku $lbsku --vnet-name $vnet --public-ip-address $pip
-extbackend=$(az network lb address-pool list -g $rg --lb-name $extlb --query [0].name -o tsv)
+extbackend=$(az network lb address-pool list -g $rg --lb-name $extlb --query '[0].name' -o tsv)
 if [[ -z "$extbackend" ]]
 then
     az network lb address-pool create -g $rg --lb-name $extlb -n $extlb-backend
@@ -40,7 +40,7 @@ fi
 az network lb probe create -g $rg --lb-name $extlb -n $extlb-probe --protocol tcp --port 22
 # Internal LB
 az network lb create -g $rg -n $intlb --sku $lbsku --vnet-name $vnet --subnet $nvasubnet
-intbackend=$(az network lb address-pool list -g $rg --lb-name $intlb --query [0].name -o tsv)
+intbackend=$(az network lb address-pool list -g $rg --lb-name $intlb --query '[0].name' -o tsv)
 if [[ -z "$intbackend" ]]
 then
     az network lb address-pool create -g $rg --lb-name $intlb -n $intlb-backend
@@ -63,22 +63,23 @@ adminpassword=$(az keyvault secret show -n $secretname --vault-name $secretskvna
 
 # Deploy VMSS (cloudinit would probably be quicker than custom script extensions)
 az vmss create -n $vmssname -g $rg -l $location --image UbuntuLTS --admin-username $adminuser --admin-password $adminpassword --lb "" --vnet-name $vnet --subnet $nvasubnet --nsg $nsgname --vm-sku $nvasku
-scripturl=https://raw.githubusercontent.com/erjosito/nva-vmss/master/linuxNVAconfig-1nic.sh
-scriptcmd="chmod 666 ./linuxNVAconfig-1nic.sh && /bin/sh ./linuxNVAconfig-1nic.sh"
+filename=linuxNVAconfig-1nic.sh
+scripturl="https://raw.githubusercontent.com/erjosito/nva-vmss/master/${filename}"
+scriptcmd="chmod 666 ./${filename} && /bin/sh ./${filename}"
 az vmss extension set -g $rg --vmss-name $vmssname -n customScript --publisher Microsoft.Azure.Extensions --settings "{'fileUris': ['$scripturl'],'commandToExecute': '$scriptcmd'}"
 
 # Configure NVA external LB (outbound rule, important to do it before the inbound rule, otherwise Intenet access is broken with the standard LB)
 # If using the basic LB, instead of an outbound rule we need a dummy LB rule
-extfrontend=$(az network lb frontend-ip list -g $rg --lb-name $extlb -o tsv --query [0].name)
-extbackend=$(az network lb address-pool list -g $rg --lb-name $extlb --query [0].name -o tsv)
-extprobe=$(az network lb probe list -g $rg --lb-name $extlb --query [0].name -o tsv)
+extfrontend=$(az network lb frontend-ip list -g $rg --lb-name $extlb -o tsv --query '[0].name')
+extbackend=$(az network lb address-pool list -g $rg --lb-name $extlb --query '[0].name' -o tsv)
+extprobe=$(az network lb probe list -g $rg --lb-name $extlb --query '[0].name' -o tsv)
 if [[ "$lbsku" == "standard" ]]
 then
     az network lb outbound-rule create -g $rg --address-pool $extbackend --frontend-ip-configs $extfrontend --idle-timeout 5 --lb-name $extlb --name outboundRule --outbound-ports 10000 --protocol Tcp
 else
     az network lb rule create -n haports -g $rg --lb-name $extlb --protocol Tcp --frontend-port 999 --backend-port 999 --frontend-ip-name $extfrontend --backend-pool-name $extbackend --probe-name $extprobe
 fi
-extbackendid=$(az network lb address-pool list -g $rg --lb-name $extlb --query [0].id -o tsv)
+extbackendid=$(az network lb address-pool list -g $rg --lb-name $extlb --query '[0].id' -o tsv)
 az vmss update -g $rg -n $vmssname --set virtualMachineProfile.networkProfile.networkInterfaceConfigurations[0].ipConfigurations[0].loadBalancerBackendAddressPools="[{\"id\":\"$extbackendid\"}]"
 az vmss update-instances -g $rg --name $vmssname --instance-ids "*"
 if [[ "$lbsku" == "standard" ]]
@@ -88,15 +89,15 @@ fi
 
 # Configure NVA external LB (inbound rule for SSH). No difference between basic and standard SKUs
 az network lb inbound-nat-pool create -g $rg --lb-name $extlb -n inboundSSH --protocol All --frontend-port-range-start 22000 --frontend-port-range-end 22009 --backend-port 22 --frontend-ip-name $extfrontend
-extpoolid=$(az network lb inbound-nat-pool list -g $rg --lb-name $extlb --query [0].id -o tsv)
+extpoolid=$(az network lb inbound-nat-pool list -g $rg --lb-name $extlb --query '[0].id' -o tsv)
 az vmss update -g $rg -n $vmssname --set virtualMachineProfile.networkProfile.networkInterfaceConfigurations[0].ipConfigurations[0].loadBalancerInboundNatPools="[{\"id\":\"$extpoolid\",\"resourceGroup\": \"$rg\"}]"
 az vmss update-instances -g $rg --name $vmssname --instance-ids "*"
 az network lb inbound-nat-rule list -g $rg --lb-name $extlb -o table
 
 # Configure NVA internal LB
-intfrontend=$(az network lb frontend-ip list -g $rg --lb-name $intlb --query [0].name -o tsv)
-intbackend=$(az network lb address-pool list -g $rg --lb-name $intlb --query [0].name -o tsv)
-intprobe=$(az network lb probe list -g $rg --lb-name $intlb --query [0].name -o tsv)
+intfrontend=$(az network lb frontend-ip list -g $rg --lb-name $intlb --query '[0].name' -o tsv)
+intbackend=$(az network lb address-pool list -g $rg --lb-name $intlb --query '[0].name' -o tsv)
+intprobe=$(az network lb probe list -g $rg --lb-name $intlb --query '[0].name' -o tsv)
 if [[ "$lbsku" == "standard" ]]
 then
     az network lb rule create -n AllPorts -g $rg --lb-name $intlb --protocol Tcp --frontend-port 0 --backend-port 0 --frontend-ip-name $intfrontend --backend-pool-name $intbackend --probe-name $intprobe
@@ -106,8 +107,8 @@ else
 fi
 az network lb rule list -g $rg --lb-name $intlb -o table
 # Don't forget to include the previously configured backend pool for the ext LB
-extbackendid=$(az network lb address-pool list -g $rg --lb-name $extlb --query [0].id -o tsv)
-intbackendid=$(az network lb address-pool list -g $rg --lb-name $intlb --query [0].id -o tsv)
+extbackendid=$(az network lb address-pool list -g $rg --lb-name $extlb --query '[0].id' -o tsv)
+intbackendid=$(az network lb address-pool list -g $rg --lb-name $intlb --query '[0].id' -o tsv)
 az vmss update -g $rg -n $vmssname --set virtualMachineProfile.networkProfile.networkInterfaceConfigurations[0].ipConfigurations[0].loadBalancerBackendAddressPools="[{\"id\":\"$extbackendid\"},{\"id\":\"$intbackendid\"}]"
 az vmss update-instances -g $rg --name $vmssname --instance-ids "*"
 
@@ -120,7 +121,7 @@ az vmss update-instances -g $rg --name $vmssname --instance-ids "*"
 az vmss show -n $vmssname -g $rg --query virtualMachineProfile.networkProfile.networkInterfaceConfigurations[0].ipConfigurations[0]
 # Instance model (after running update-instances the vmss model is propagated to the instance model):
 # Take the first instance ID
-instanceid=$(az vmss list-instances -g $rg -n $vmssname --query [0].instanceId -o tsv)
+instanceid=$(az vmss list-instances -g $rg -n $vmssname --query '[0].instanceId' -o tsv)
 # LB pools
 az vmss show -g $rg -n $vmssname --instance-id $instanceid --query networkProfileConfiguration.networkInterfaceConfigurations[0].ipConfigurations[0]
 # IP forwarding:
